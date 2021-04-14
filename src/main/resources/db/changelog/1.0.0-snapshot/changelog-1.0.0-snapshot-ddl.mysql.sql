f0700cb02a48ec192be6b49a289c0656b805a3b0
Pettyfer <pettyferlove@live.cn>
2021-04-14 15:01:14 +0800

    :wrench: 完善部署时一致性问题，增加环境变量设置

--- src/main/resources/db/changelog/1.0.0-snapshot/changelog-1.0.0-snapshot-ddl.mysql.sql
+++ src/main/resources/db/changelog/1.0.0-snapshot/changelog-1.0.0-snapshot-ddl.mysql.sql
@@ -17,3 +17,24 @@ alter table biz_application_deployment_network add env_type tinyint unsigned def
 
 --changeset Petty:caas-1.0.0-snapshot-ddl-3
 alter table biz_application_deployment add environment_variable varchar(1000) null comment '应用环境变量' after image_tag;
+
+--changeset Petty:caas-1.0.0-snapshot-ddl-4
+create table biz_application_deployment_volume
+(
+    id            varchar(128)     not null
+        primary key,
+    deployment_id varchar(128)     null,
+    volume_name   varchar(128)     null comment '卷名',
+    volume_type   varchar(255)     null comment '卷类型（配置文件、目录、空目录、主机目录）',
+    volume_path   varchar(255)     null comment '卷地址（空目录和配置文件不需指定）',
+    config_id     varchar(128)     null comment '配置文件ID',
+    mount_name    varchar(128)     null comment '挂载名称',
+    mount_path    varchar(255)     null comment '挂载地址',
+    del_flag      bit default b'0' null comment '删除标记 0 未删除 1 删除',
+    creator       varchar(128)     null comment '创建人',
+    create_time   datetime         null comment '创建时间',
+    modifier      varchar(128)     null comment '修改人',
+    modify_time   datetime         null comment '修改时间',
+    group_id      varchar(128)     null comment '项目组ID',
+    tenant_id     varchar(128)     null comment '租户ID'
+);
---
+++ tools/build.ps1
@@ -0,0 +1,92 @@
+$prefix="192.168.0.4/tools"
+$version="1.0.0"
+$jdk_version="oracle-jdk-8_281"
+$jre_version="oracle-jre-8_281"
+# 基础镜像
+Write-Output "Begin building the base tool image,version:$version"
+Set-Location "alpine-base"
+docker build -t "$prefix/alpine-base:$version" .
+docker push "$prefix/alpine-base:$version"
+Set-Location "../"
+Write-Output "The base tool image has been built"
+
+# 基础JDK镜像
+Write-Output "Begin building the oracle jdk image,version:$jdk_version"
+Set-Location "alpine-oracle-jdk-8"
+docker build -t "$prefix/alpine-tool-base:$jdk_version" .
+docker push "$prefix/alpine-tool-base:$jdk_version"
+Set-Location "../"
+Write-Output "The oracle jdk image has been built"
+
+# 基础JRE镜像
+Write-Output "Begin building the oracle jre image,version:$jre_version"
+Set-Location "alpine-oracle-jre-8"
+docker build -t "$prefix/alpine-tool-base:$jre_version" .
+docker push "$prefix/alpine-tool-base:$jre_version"
+Set-Location "../"
+Write-Output "The oracle jre image has been built"
+
+# Docker构建镜像
+Write-Output "Begin building the docker build image,version:$version"
+Set-Location "build-docker-tool"
+docker build -t "$prefix/build-docker-tool:$version" .
+docker push "$prefix/build-docker-tool:$version"
+Set-Location "../"
+Write-Output "The docker build image has been built"
+
+# Git工具镜像
+Write-Output "Begin building the git tool image,version:$version"
+Set-Location "build-git-tool"
+docker build -t "$prefix/build-git-tool:$version" .
+docker push "$prefix/build-git-tool:$version"
+Set-Location "../"
+Write-Output "The git tool image has been built"
+
+
+# Maven工具镜像
+Write-Output "Begin building the maven tool image,version:$version"
+Set-Location "build-maven-tool"
+docker build -t "$prefix/build-maven-tool:$version" .
+docker push "$prefix/build-maven-tool:$version"
+Set-Location "../"
+Write-Output "The maven tool image has been built"
+
+# NodeJS工具镜像
+Write-Output "Begin building the nodejs tool image,version:$version"
+Set-Location "build-nodejs-tool"
+docker build -t "$prefix/build-nodejs-tool:$version" .
+docker push "$prefix/build-nodejs-tool:$version"
+Set-Location "../"
+Write-Output "The nodejs tool image has been built"
+
+# Notification工具镜像
+Write-Output "Begin building the notification tool image,version:$version"
+Set-Location "build-notification-tool"
+docker build -t "$prefix/build-notification-tool:$version" .
+docker push "$prefix/build-notification-tool:$version"
+Set-Location "../"
+Write-Output "The notification tool image has been built"
+
+# Persistence工具镜像
+Write-Output "Begin building the persistence tool image,version:$version"
+Set-Location "build-persistence-tool"
+docker build -t "$prefix/build-persistence-tool:$version" .
+docker push "$prefix/build-persistence-tool:$version"
+Set-Location "../"
+Write-Output "The persistence tool image has been built"
+
+# SQL工具镜像
+Write-Output "Begin building the sql tool image,version:$version"
+Set-Location "build-sql-tool"
+docker build -t "$prefix/build-sql-tool:$version" .
+docker push "$prefix/build-sql-tool:$version"
+Set-Location "../"
+Write-Output "The sql tool image has been built"
+
+# Subversion
+Write-Output "Begin building the subversion tool image,version:$version"
+Set-Location "build-subversion-tool"
+docker build -t "$prefix/build-subversion-tool:$version" .
+docker push "$prefix/build-subversion-tool:$version"
+Set-Location "../"
+Write-Output "The subversion tool image has been built"
